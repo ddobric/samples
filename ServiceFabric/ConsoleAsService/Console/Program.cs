95b67d6f12f72ceba77cd91934833894cad95e5b
Damir Dobric <ddobric@daenet.de>
2016-01-10 23:10:45 +0100

    initial working version

---
+++ ServiceFabric/ConsoleAsService/Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+
+namespace Console
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            try
+            {
+                System.Console.WriteLine("Console started");
+                if (args != null)
+                {
+                    foreach (var arg in args)
+                    {
+                        System.Console.WriteLine(arg);
+                    }
+                }
+
+                // Creating a FabricRuntime connects this host process to the Service Fabric runtime.
+                using (FabricRuntime fabricRuntime = FabricRuntime.Create())
+                {
+                    while (true)
+                    {
+                        System.Console.WriteLine("Running at {0}", DateTime.Now );
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
