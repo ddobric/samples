1688de15033e0fc0ef6927385f2e3cea9306738f
Damir Dobric <ddobric@daenet.de>
2016-01-16 19:29:36 +0100

    v0

---
+++ ServiceFabric/StateLessService/StatelessSvc/ServiceBusQueueListener.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Fabric;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace StatelessSvc
+{
+    public class ServiceBusQueueListener : ICommunicationListener
+    {
+        private StatelessServiceInitializationParameters m_Args;
+        private StatelessService m_Service;
+
+        public ServiceBusQueueListener(StatelessService service, StatelessServiceInitializationParameters args)
+        {
+            this.m_Service = service;
+            this.m_Args = args;
+        }
+
+        public void Abort()
+        {
+            ServiceEventSource.Current.ServiceMessage(m_Service, "ServiceBusQueueListener.Abort invoked ");
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            ServiceEventSource.Current.ServiceMessage(m_Service, "ServiceBusQueueListener.CloseAsync invoked ");
+            return Task.Run(() =>{ });
+        }
+
+ 
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            ServiceEventSource.Current.ServiceMessage(m_Service, "ServiceBusQueueListener.OpenAsync invoked ");
+
+            return Task<string>.Run(() => { return "samplequeue"; });
+        }
+    }
+}
---
+++ ServiceFabric/StateLessService/StatelessSvc/StatelessSvc.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StatelessSvc
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class StatelessSvc : StatelessService
+    {
+
+      
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+        
+            // TODO: If your service needs to handle user requests, return a list of ServiceReplicaListeners here.
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener((args)=> new ServiceBusQueueListener(this, args) ),
+            };
+        }
+
+
+        
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancelServiceInstance">Canceled when Service Fabric terminates this instance.</param>
+        protected override async Task RunAsync(CancellationToken cancelServiceInstance)
+        {
+           
+            // TODO: Replace the following sample code with your own logic.
+            var a = this.ServiceInitializationParameters.CodePackageActivationContext.GetConfigurationPackageNames();
+            var pkg = this.ServiceInitializationParameters.CodePackageActivationContext.GetConfigurationPackageObject("Config");
+
+
+            int iterations = 0;
+            // This service instance continues processing until the instance is terminated.
+            while (!cancelServiceInstance.IsCancellationRequested)
+            {
+
+                // Log what the service is doing
+                ServiceEventSource.Current.ServiceMessage(this, "v1.0.8. Working-{0}", iterations++);
+
+                // Pause for 1 second before continue processing.
+                await Task.Delay(TimeSpan.FromSeconds(1), cancelServiceInstance);
+            }
+        }
+    }
+}
