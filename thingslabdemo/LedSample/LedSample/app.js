032ab35bc8cbc0ddd94788c255c0ac57b04d3786
Damir Dobric <ddobric@daenet.de>
2016-02-25 09:46:10 +0100

    LED Sample

---
+++ thingslabdemo/LedSample/LedSample/app.js
@@ -0,0 +1,25 @@
+﻿//https://github.com/rwaldron/johnny-five/wiki/Getting-Started
+
+var five = require("johnny-five"),
+    board = new five.Board();
+
+board.on("ready", function () {
+    
+    var delay = 250;
+    
+    // Create an Led on pin 10
+    var led = new five.Led(10);
+    
+    function doIt() {
+        
+        board.wait(delay, function () {
+            if (led.isOn)
+                led.off();
+            else
+                led.on();
+        });
+    }
+    
+    board.loop(4500, doIt);
+});
+
