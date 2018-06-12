3e28df337f15c93746ffc5ca568de8b72f95b1d6
Damir Dobric <ddobric@daenet.com>
2018-06-12 14:29:47 +0200

    .

--- cosmos-perftests/CosmosPerfTests/CosmosPerfTests/Credentials.cs
+++ cosmos-perftests/CosmosPerfTests/CosmosPerfTests/Credentials.cs
@@ -11,7 +11,7 @@ namespace CosmosPerfTests
         public static class MongoDb
         {
 
-            public const string ConnectionString = @"mongodb://daenetcosmos:I4pxJURbOxdtaWe8abcicqqDRnMNfZob0mhNS8HNT37zDdlvGOVHdGmemuXWeSYCS2MDtOno9l3brKMMrnDeJQ==@daenetcosmos.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+            public const string ConnectionString = @"";
             public const string UserName = "";
             public const string Host = "";
             public const string Password = "";
@@ -19,8 +19,8 @@ namespace CosmosPerfTests
 
         public static class DocumentDb
         {
-            public const string EndpointUri = "https://daenet-sample.documents.azure.com:443/";
-            public const string Key = "fZ1CirSlslScxUbVRcyfY8g9b9kzpKJnLAhskEQaHkLmPT9HHwPeZBMM4muruZ9z35oySmbsOvYDTpYUcvOrzg==";
+            public const string EndpointUri = "";
+            public const string Key = "";
             public const string DatabaseName = "testdb";
             public const string CollectionName = "collection1";
         }
