b7260aca7bd88115cb64b04190bcfc505a53bea3
Damir Dobric <ddobric@daenet.com>
2019-05-18 13:22:16 +0200

    Azure Configuration Service

---
+++ configservice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+
+namespace configservice
+{
+    // See: https://docs.microsoft.com/en-us/azure/azure-app-configuration/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+
+            var config = builder.Build();
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        }
+    }
+}
